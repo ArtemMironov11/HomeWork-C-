fd92d7b47ece4bf605f94c6e6a4b5ad2f4e196fa
Tatiana Mironova <tatiananikolaeva@MacBook-Pro-Tatiana.local>
2022-09-05 14:12:13 +0300

    Task1 HW-4

---
+++ HomeWork4/Task1/Task1-4hw.cs
@@ -0,0 +1,23 @@
+﻿
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Create numbers A & B
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+// Checking for 0
+
+if (B == 0)
+{
+    A = A^B;
+    Console.WriteLine("Result = " + A);
+}
+else 
+{
+    int tmp = A;
+    for (i = 0; i < B-1; i++)
+    {
+        A = A * tmp;
+    }
+    Console.WriteLine("Result = " + A);
+}
