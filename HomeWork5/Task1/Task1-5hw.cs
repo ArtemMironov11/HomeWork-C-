b4bac6b70c515c844ebf1d7de917c19fc82aed50
Tatiana Mironova <tatiananikolaeva@MacBook-Pro-Tatiana.local>
2022-09-08 14:07:43 +0300

    HW5

---
+++ HomeWork5/Task1/Task1-5hw.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+// create new array
+int start  = 100;
+int stop = 1000;
+int count = 0;
+
+Console.WriteLine("Ввести длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [length];
+
+// Starting
+for (int i=0; i<length; i++)
+{
+    array[i] = new Random().Next(start, stop);
+    Console.Write(array[i] + ",");
+    if (array[i] %2 == 0){count += 1;}
+}
+
+Console.WriteLine("Колическтво четных чисел в массиве = " + count);
---
+++ HomeWork5/Task2/Task2-5hw.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму четных элементов, стоящих на нечётных позициях 
+// (позиция - индекс элемента в массиве).
+
+
+Console.WriteLine("Ввести длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [length];
+
+int Sum = 0;
+
+for (int i=0; i<length; i++)
+{
+    array[i] = new Random().Next(1, 1000);
+    Console.Write(array[i] + ",");
+    if (array[i] % 2 == 0 && i % 2 != 0)
+    {
+        Sum = Sum + array[i];
+    }
+}
+Console.WriteLine("Сумма четных элементов, стоящих на нечётных позициях = " + Sum);
