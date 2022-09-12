2eda7666d61285705e6db3b36c4d9ed440948c36
Tatiana Mironova <tatiananikolaeva@MacBook-Pro-Tatiana.local>
2022-09-12 12:46:15 +0300

    HW6

---
+++ HomeWork6/Task1/task1-6hw.cs
@@ -0,0 +1,28 @@
+﻿// Программа запрашивает натуральное число M, 
+// пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+//Random number М
+int M = new Random().Next(1, 11);
+
+Console.WriteLine("Вводите числа " + M + " раз: ");
+
+int[] array = new int [M];
+
+for (int i = 0; i < M; i++)
+{
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+
+for (int i = 0; i < M; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
